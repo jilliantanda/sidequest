424da90f492d29332610f49bcfbd92eb5b02d299
Jillian Tanda <jilliantanda@gmail.com>
2024-06-15 15:10:28 -0400

    added new.ejs page to display all Schema info

--- controllers/quests.js
+++ controllers/quests.js
@@ -3,8 +3,30 @@ const router = express.Router()
 const Quest = require('../models/quests.js')
 
 router.get('/', async (req, res) => {
-    const allQuests = await Quest.find({})
     res.render('index.ejs')
 })
 
+router.get('/questlog/', async (req, res) => {
+        const allQuests = await Quest.find({})
+    res.render('index.ejs', {
+        quests: allQuests
+    })
+})
+
+router.get('/newquest/', (req, res) => {
+    res.render('new.ejs')
+})
+
+router.put('/:id', async (req, res) => {
+    if(req.body.isCompleted === "on"){
+        req.body.isCompleted = true
+    } else {
+        req.body.isCompleted = false
+    } try {
+        const updatedQuest = await Quest.findByIdandUpdate(req.params.id, req.body, { new: true })
+        res.redirect('/questlog/')
+    } catch (err){
+        console.error(err)
+    }
+})
 module.exports = router;
--- models/quests.js
+++ models/quests.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const questSchema = new Schema ({
-    name: { type: String, required: true },
-    descritipn: { type: String, required: true},
-    type: Boolean
+    title: { type: String, required: true },
+    descritipn: { type: String, required: true },
+    questType: { type: String, enum: ['Travel', 'Experience', 'Life Goal']},
+    imageUrl: { type: String, required: true },
+    isCompleted: { type: Boolean, default: false }
 })
+
 
 const Quest = mongoose.model('Quest', questSchema)
 
